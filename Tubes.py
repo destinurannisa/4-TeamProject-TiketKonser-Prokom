7ab52e110fee2ae12697431f0e4654b87c0b1ead
Andre Firmansyah <andrefirmansyah966@gmail.com>
2022-06-11 19:35:07 +0700

    Membuat_Fungsi

--- Tubes.py
+++ Tubes.py
@@ -13,7 +13,7 @@ def login():
         print("Data pengguna tidak ditemukan dalam database")
         input_valid = False
         while (input_valid == False):
-            Tidak_punya_akun = input("Apakah anda memang tidak memiliki akun?(Y/N) ")
+            Tidak_punya_akun = input("Apakah anda memang tidak memiliki akun? (Y/N) ")
             if Tidak_punya_akun == "Y":
                 print("Data anda akan disimpan dalam database sebagai akun anda")
                 print("Mohon lakukan kembali validasi data")
@@ -31,106 +31,74 @@ def login():
 login()
 #Bagian validasi data selesai
 
-pemesanan_ulang = True
-while (pemesanan_ulang == True):
-    print("Tampilan menu")
-    #muncul gambar tabel
-    input_valid = False
-    while (input_valid == False) :
-        Pemesanan = input("Apakah anda ingin melakukan pemesanan? (Y/N) ")
-        if Pemesanan == "Y":
-            input_valid = True
-            pemesanan_ulang = False
-            print("Menampilkan gambar seat kursi penonton")
-            print("Menampilkan tabel harga dan sisa kursi")
-        elif Pemesanan == "N":
-            input_valid = True
-            print("END")
-            exit()
-        else:
-            input_valid = False
-            print("Input tidak valid")
-            print("Mohon melakukan input ulang")
-    input_valid = False
-    while (input_valid == False) :
-        Tiket = input("Jenis tiket yang anda pilih adalah : ")
-        if Tiket == "VVIP":
-            input_valid = True
-            Jumlah_tiket = int(input("Berapa jumlah tiket yang akan dibeli? "))
-            Total_Bayar = 5000000 * Jumlah_tiket
-            print("Total Bayar adalah ", Total_Bayar)
-        elif Tiket == "VIP":
-            input_valid = True
-            Jumlah_tiket = int(input("Berapa jumlah tiket yang akan dibeli? "))
-            Total_Bayar = 4000000 * Jumlah_tiket
-            print("Total Bayar adalah ", Total_Bayar)
-        elif Tiket == "A":
-            input_valid = True
-            Jumlah_tiket = int(input("Berapa jumlah tiket yang akan dibeli? "))
-            Total_Bayar = 3000000 * Jumlah_tiket
-            print("Total Bayar adalah ", Total_Bayar)
-        elif Tiket == "B":
-            input_valid = True
-            Jumlah_tiket = int(input("Berapa jumlah tiket yang akan dibeli? "))
-            Total_Bayar = 2500000 * Jumlah_tiket
-            print("Total Bayar adalah ", Total_Bayar)
-        elif Tiket == "VIP":
-            input_valid = True
-            Jumlah_tiket = int(input("Berapa jumlah tiket yang akan dibeli? "))
-            Total_Bayar = 2000000 * Jumlah_tiket
-            print("Total Bayar adalah ", Total_Bayar)
-        else:
-            input_valid = False
-            print("Input tidak valid")
-            print("Mohon melakukan input ulang")
+def pemesanan():
+    Pemesanan = input("Apakah anda ingin melakukan pemesanan? (Y/N) ")
+    if Pemesanan == "Y" or "y":
+        print("Menampilkan gambar seat kursi penonton")
+        print("Menampilkan tabel harga dan sisa kursi")
+    elif Pemesanan == "N" or "n":
+        print("END")
+        exit()
+    else:
+        print("Input tidak valid")
+        print("Mohon melakukan input ulang")
+        return pemesanan()
+pemesanan()
 
-    input_valid = False
-    while (input_valid == False) :
-        Metode_Pembayaran = input("Metode pembayaran apa yang akan anda gunakan? ")
-        if Metode_Pembayaran == "tunai":
-            input_valid = True
-            Uang_Pembayaran = int(input("Nominal yang dikeluarkan? "))
-            Kembalian = (Uang_Pembayaran - Total_Bayar)
-            print("Jumlah kembalian adalah ", Kembalian)
-            print("Terimakasih telah melakukan pembayaran")
-        elif Metode_Pembayaran == "OVO":
-            input_valid = True
-            Nominal_Bayar = int(input("Nominal yang anda keluarkan adalah :"))
-            print("Terimakasih telah melakukan pembayaran")
-        elif Metode_Pembayaran == "Gopay":
-            input_valid = True
-            Nominal_Bayar = int(input("Nominal yang anda keluarkan adalah :"))
-            print("Terimakasih telah melakukan pembayaran") 
-        elif Metode_Pembayaran == "Shopeepay":
-            input_valid = True
-            Nominal_Bayar = int(input("Nominal yang anda keluarkan adalah :"))
-            print("Terimakasih telah melakukan pembayaran")
-        elif Metode_Pembayaran == "Credit Card":
-            input_valid = True
-            Nominal_Bayar = int(input("Nominal yang anda keluarkan adalah :"))
-            print("Terimakasih telah melakukan pembayaran")
-        elif Metode_Pembayaran == "Debit":
-            input_valid = True
-            Nominal_Bayar = int(input("Nominal yang anda keluarkan adalah :"))
+def tiket():
+    Tiket = input("Jenis tiket yang anda pilih adalah : ")
+    if Tiket == "VVIP":
+        Jumlah_tiket = int(input("Berapa jumlah tiket yang akan dibeli? "))
+        Total_Bayar = 5000000 * Jumlah_tiket
+        print("Total Bayar adalah ", Total_Bayar)
+    elif Tiket == "VIP":
+        Jumlah_tiket = int(input("Berapa jumlah tiket yang akan dibeli? "))
+        Total_Bayar = 4000000 * Jumlah_tiket
+        print("Total Bayar adalah ", Total_Bayar)
+    elif Tiket == "A":
+        Jumlah_tiket = int(input("Berapa jumlah tiket yang akan dibeli? "))
+        Total_Bayar = 3000000 * Jumlah_tiket
+        print("Total Bayar adalah ", Total_Bayar)
+    elif Tiket == "B":
+        Jumlah_tiket = int(input("Berapa jumlah tiket yang akan dibeli? "))
+        Total_Bayar = 2500000 * Jumlah_tiket
+        print("Total Bayar adalah ", Total_Bayar)
+    elif Tiket == "VIP":
+        Jumlah_tiket = int(input("Berapa jumlah tiket yang akan dibeli? "))
+        Total_Bayar = 2000000 * Jumlah_tiket
+        print("Total Bayar adalah ", Total_Bayar)
+    else:
+        print("Input tidak valid")
+        print("Mohon melakukan input ulang")
+        return tiket()
+tiket()
+def pembayaran():
+    Metode_Pembayaran = input("Metode pembayaran apa yang akan anda gunakan? ")
+    if Metode_Pembayaran == "tunai":
+        Uang_Pembayaran = int(input("Nominal yang dikeluarkan? "))
+        Kembalian = (Uang_Pembayaran - tiket)
+        print("Jumlah kembalian adalah ", Kembalian)
+        print("Terimakasih telah melakukan pembayaran")
+    elif Metode_Pembayaran == "OVO" or "Gopay" or "Shopeepay" or "Credit Card" or "Debit":
+        Nominal_Bayar = int(input("Nominal yang anda keluarkan adalah :"))
+        if Nominal_Bayar == tiket:
             print("Terimakasih telah melakukan pembayaran")
         else:
-            input_valid = False
-            print("Input tidak valid")
-            print("Mohon melakukan input ulang")
-
-    input_valid = False
-    while (input_valid == False):
-        Transaksi_Lagi = input("Apakah anda ingin melakukan transaksi lagi? ")
-        if Transaksi_Lagi == "Y":
-            input_valid = True
-            pemesanan_ulang = True
-        elif Transaksi_Lagi == "N":
-            input_valid = True
-            print("Selesai")
-            exit()
-        else:
-            input_valid = False
-            print("Input tidak valid")
-            print("Mohon melakukan input ulang")
-
-
+            return Nominal_Bayar
+    else:
+        print("Input tidak valid")
+        print("Mohon melakukan input ulang")
+        return pembayaran()
+pembayaran()
+def transaksi_lain():
+    Transaksi_Lagi = input("Apakah anda ingin melakukan transaksi lagi? ")
+    if Transaksi_Lagi == "Y":
+        return pemesanan()
+    elif Transaksi_Lagi == "N":
+        print("Selesai")
+        exit()
+    else:        
+        print("Input tidak valid")
+        print("Mohon melakukan input ulang")
+        return transaksi_lain()
+transaksi_lain()
