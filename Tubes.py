415731905fce0852c83f362f33f95e82574dd53b
Desti Nur Annisa <destiannisa@student.uns.ac.id>
2022-06-12 19:21:38 +0700

    12 Juni

--- Tubes.py
+++ Tubes.py
@@ -71,20 +71,59 @@ def tiket():
         print("Input tidak valid")
         print("Mohon melakukan input ulang")
         return tiket()
+def tiket():
+    Tiket = input("Jenis tiket yang anda pilih adalah : ")
+    if Tiket == "VVIP":
+        Jumlah_tiket = int(input("Berapa jumlah tiket yang akan dibeli? "))
+        Total_Bayar = 5000000 * Jumlah_tiket
+        print("Total Bayar adalah ", Total_Bayar)
+    elif Tiket == "VIP":
+        Jumlah_tiket = int(input("Berapa jumlah tiket yang akan dibeli? "))
+        Total_Bayar = 4000000 * Jumlah_tiket
+        print("Total Bayar adalah ", Total_Bayar)
+    elif Tiket == "A":
+        Jumlah_tiket = int(input("Berapa jumlah tiket yang akan dibeli? "))
+        Total_Bayar = 3000000 * Jumlah_tiket
+        print("Total Bayar adalah ", Total_Bayar)
+    elif Tiket == "B":
+        Jumlah_tiket = int(input("Berapa jumlah tiket yang akan dibeli? "))
+        Total_Bayar = 2500000 * Jumlah_tiket
+        print("Total Bayar adalah ", Total_Bayar)
+    elif Tiket == "VIP":
+        Jumlah_tiket = int(input("Berapa jumlah tiket yang akan dibeli? "))
+        Total_Bayar = 2000000 * Jumlah_tiket
+        print("Total Bayar adalah ", Total_Bayar)
+    else:
+        print("Input tidak valid")
+        print("Mohon melakukan input ulang")
+        return tiket()
 tiket()
 def pembayaran():
+    print("Anda dapat membayar dengan metode tunai/OVO/Gopay/Shopeepay/Credit Card/Debit")
     Metode_Pembayaran = input("Metode pembayaran apa yang akan anda gunakan? ")
     if Metode_Pembayaran == "tunai":
         Uang_Pembayaran = int(input("Nominal yang dikeluarkan? "))
-        Kembalian = (Uang_Pembayaran - tiket)
+        Kembalian = (Uang_Pembayaran - (Total_Bayar))
         print("Jumlah kembalian adalah ", Kembalian)
         print("Terimakasih telah melakukan pembayaran")
-    elif Metode_Pembayaran == "OVO" or "Gopay" or "Shopeepay" or "Credit Card" or "Debit":
+    elif Metode_Pembayaran == "OVO":
         Nominal_Bayar = int(input("Nominal yang anda keluarkan adalah :"))
         if Nominal_Bayar == tiket:
-            print("Terimakasih telah melakukan pembayaran")
+            print("Terima kasih telah melakukan pembayaran")
         else:
-            return Nominal_Bayar
+            return Nominal_Bayar()
+    elif Metode_Pembayaran == "Gopay":
+        Nominal_Bayar = int(input("Nominal yang anda keluarkan adalah :"))
+        print("Terima kasih telah melakukan pembayaran")
+    elif Metode_Pembayaran == "Shopeepay":
+        Nominal_Bayar = int(input("Nominal yang anda keluarkan adalah :"))
+        print("Terima kasih telah melakukan pembayaran")
+    elif Metode_Pembayaran == "Credit Card":
+        Nominal_Bayar = int(input("Nominal yang anda keluarkan adalah :"))
+        print("Terima kasih telah melakukan pembayaran")
+    elif Metode_Pembayaran == "Debit/Credit":
+        Nominal_Bayar = int(input("Nominal yang anda keluarkan adalah :"))
+        print("Terima kasih telah melakukan pembayaran")
     else:
         print("Input tidak valid")
         print("Mohon melakukan input ulang")
